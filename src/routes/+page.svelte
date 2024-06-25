1fa3ebb4072f0e78b00841679607f9fa85c2e31f
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-06-25 19:44:30 +0200

    update options clear

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,59 @@
-<main>
-  <h1>SvelteKit Auth Example</h1>
-  <p>
-    This is an example site to demonstrate how to use <a
-      href="https://kit.svelte.dev/">SvelteKit</a
-    >
-    with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
-  </p>
-</main>
+<script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
+  import { hoverable } from '$UITools/Cursor/cursorHelpers'
+  import { t } from '$UITools/Translations/index'
+  import gsap from 'gsap'
+  
+	import Box from './../components/three/Box.svelte';
+  import Slider from '$components/Slider.svelte'
+  import { setTransitionLoader } from '$stores/UX/transitionLoaderStore'
+  import { fetchMockData } from '$lib/utils/mockService'
+  import { enter, exit } from './transition'
+  import { isAnimatingOut } from '$stores/UX/isAnimating'
+  import { onNavigate } from '$app/navigation'
+
+  const linkUrl: string = 'https://kit.svelte.dev'
+  let title: HTMLElement
+  let text: HTMLElement
+  let link: HTMLElement
+  let path: string
+
+  onNavigate((navigation) => {
+    path = navigation.to?.route.id
+  })
+
+  onMount(() => {
+    setTransitionLoader(false)
+    fetchMockData
+  })
+</script>
+
+<svelte:head>
+  <title>Page d'exemple</title>
+  <meta
+    name="description"
+    content="Ceci est une description de la page d'exemple."
+  />
+</svelte:head>
+
+<div
+  class="home"
+  in:enter="{{ duration: 1, title, text, link }}"
+  out:exit="{{ duration: 1, title, text, link }}"
+>
+  <h1 bind:this="{title}">{$t('data.home-title')}</h1>
+  <p bind:this="{text}">{@html $t('data.home-text', { linkUrl })}</p>
+  <div class="linkhome" bind:this="{link}">
+    <a href="/about" use:hoverable="{'first'}">{$t('data.home-link')}</a>
+  </div>
+  <Slider />
+  <Box />
+</div>
 
 <style>
-  main {
-    flex: 1;
+  .home {
+    width: 100vw;
+    position: absolute;
+    background-color: rgb(150, 150, 150);
   }
 </style>
